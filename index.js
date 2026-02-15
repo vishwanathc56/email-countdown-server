bcbe8794cd20708226f2000680385ff9dbb2d75d
vishwa <vishwa@aticara.com>
2026-02-15 18:27:00 +0530

    remd port ine and added module.exports

--- index.js
+++ index.js
@@ -197,10 +197,14 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("✅ MongoDB connected");
 
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
+        /*   app.listen(PORT, () => {
+              console.log(`Server running on port ${PORT}`);
+          }); */
     })
     .catch((err) => {
         console.error("❌ MongoDB connection error:", err);
     });
+
+
+module.exports = app;
+
